222cf2dd0397ad62e0fae9ba311eb2f5921f639a
xiaxichen <xiaxichen.com>
2020-05-10 13:17:55 +0800

    视图函数

---
+++ crontab/master/api/api.go
@@ -0,0 +1,103 @@
+package api
+
+import (
+	"distributedSchedulingTask/crontab/common"
+	"distributedSchedulingTask/crontab/master"
+	"distributedSchedulingTask/crontab/master/handler"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+// HandlerJobSave:保存任务
+func HandlerJobSave(ctx *gin.Context) error {
+	data := &common.Job{}
+	err := ctx.BindJSON(data)
+	if err != nil {
+		return handler.ParameterError("Post Data Err")
+	}
+	job, err := master.G_jobMgr.SaveJob(data)
+	if err != nil {
+		return err
+	}
+	ctx.JSON(http.StatusOK, common.Response{Error: 0, Msg: "", Data: job})
+	return nil
+}
+
+// HandlerJobDelete:删除任务
+func HandlerJobDelete(ctx *gin.Context) error {
+	data := &common.DelJob{}
+	err := ctx.BindJSON(data)
+	if err != nil {
+		return handler.ParameterError("Post Data Err")
+	}
+	job, err := master.G_jobMgr.DeleteJob(data)
+	if err != nil {
+		return err
+	}
+	ctx.JSON(http.StatusOK, common.Response{Error: 0, Msg: "", Data: job})
+	return nil
+}
+
+// HandlerJobList:任务列表
+func HandlerJobList(ctx *gin.Context) error {
+	jobs, err := master.G_jobMgr.ListJob()
+	if err != nil {
+		return err
+	}
+	ctx.JSON(http.StatusOK, common.Response{Error: 0, Msg: "", Data: jobs})
+	return nil
+}
+
+// HandleWorkerList:获取健康worker节点列表
+func HandleWorkerList(ctx *gin.Context) error {
+
+	workerArr, err := master.G_workerMgr.ListWorkers()
+	if err != nil {
+		return err
+	}
+
+	// 正常应答
+	ctx.JSON(http.StatusOK, common.Response{Error: 0, Msg: "", Data: workerArr})
+	return nil
+}
+
+// HandlerJobLog:获取任务日志
+func HandlerJobLog(ctx *gin.Context) error {
+	name, b := ctx.GetQuery("name")
+	skipParam := ctx.DefaultQuery("skip", "0")
+	skip, err := strconv.Atoi(skipParam)
+	if err != nil {
+		skip = 0
+	}
+	limitParam := ctx.DefaultQuery("limit", "0")
+	limit, err := strconv.Atoi(limitParam)
+	if err != nil {
+		skip = 0
+	}
+	if b {
+		if log, err := master.G_logMgr.SelectListLog(name, skip, limit); err != nil {
+			return err
+		} else {
+			ctx.JSON(http.StatusOK, common.Response{Error: 0, Msg: "", Data: log})
+		}
+	} else {
+		ctx.JSON(http.StatusOK, common.Response{Error: -1, Msg: "param error!", Data: nil})
+	}
+	return nil
+}
+
+// HandlerKiller:杀死任务
+func HandlerKiller(ctx *gin.Context) error {
+	query, b := ctx.GetQuery("name")
+	if b {
+		err := master.G_jobMgr.KillJob(query)
+		if err != nil {
+			return err
+		}
+		ctx.JSON(http.StatusOK, common.Response{Error: 0, Msg: "", Data: nil})
+	} else {
+		ctx.JSON(http.StatusOK, common.Response{Error: -1, Msg: "param error!", Data: nil})
+	}
+	return nil
+}
